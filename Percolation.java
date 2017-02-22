8c7302b9a7344501e43d7dd9f0f23df48e72c425
Angelleal <angeleal14@gmail.com>
2017-02-21 22:53:27 -0400

    Added PercolationStats Class

--- Percolation.java
+++ Percolation.java
@@ -6,7 +6,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     int gridSize = 0, openSiteCount = 0, virtualTop = 0, virtualBottom = 0;
     boolean[] openSites;
-    WeightedQuickUnionUF sites;
+    WeightedQuickUnionUF sites, sitesViz;
 
     /**
      * create n-by-n grid, with all sites blocked
@@ -14,13 +14,18 @@ public class Percolation {
      * @param n grid size
      */
     public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("N must be greater than 0");
+        }
         gridSize = n;
         int siteNumber = n * n;
         virtualBottom = siteNumber + 1;
         openSites = new boolean[siteNumber + 2];
         sites = new WeightedQuickUnionUF(siteNumber + 2);
+        sitesViz = new WeightedQuickUnionUF(siteNumber + 1);
         for (int i = 1; i <= gridSize; i++) {
             sites.union(virtualTop, i);
+            sitesViz.union(virtualTop, i);
             int bottomRowStart = virtualBottom - gridSize;
             sites.union(virtualBottom, bottomRowStart + i);
         }
@@ -32,7 +37,7 @@ public class Percolation {
      * @param row row of site
      * @param col column of site
      */
-    public void open(int row, int col) {
+    public void open(int row, int col) throws ArrayIndexOutOfBoundsException {
         connectTopSite(row, col);
         connectRightSite(row, col);
         connectBottomSite(row, col);
@@ -48,7 +53,7 @@ public class Percolation {
      * @param col
      * @return
      */
-    public boolean isOpen(int row, int col) {
+    public boolean isOpen(int row, int col) throws ArrayIndexOutOfBoundsException {
         return openSites[rowColTo1D(row, col)];
     }
 
@@ -59,8 +64,8 @@ public class Percolation {
      * @param col
      * @return
      */
-    public boolean isFull(int row, int col) {
-        return isOpen(row, col) && sites.connected(virtualTop, rowColTo1D(row, col));
+    public boolean isFull(int row, int col) throws ArrayIndexOutOfBoundsException {
+        return isOpen(row, col) && sitesViz.connected(virtualTop, rowColTo1D(row, col));
     }
 
     /**
@@ -86,22 +91,34 @@ public class Percolation {
     }
 
     private void connectLeftSite(int row, int col) {
-        if (col > 1 && isOpen(row, col - 1))
+        if (col > 1 && isOpen(row, col - 1)) {
             sites.union(rowColTo1D(row, col), rowColTo1D(row, col - 1));
+            sitesViz.union(rowColTo1D(row, col), rowColTo1D(row, col - 1));
+        }
+
     }
 
     private void connectRightSite(int row, int col) {
-        if (col < gridSize && isOpen(row, col + 1))
+        if (col < gridSize && isOpen(row, col + 1)) {
             sites.union(rowColTo1D(row, col), rowColTo1D(row, col + 1));
+            sitesViz.union(rowColTo1D(row, col), rowColTo1D(row, col + 1));
+        }
+
     }
 
     private void connectTopSite(int row, int col) {
-        if (row > 1 && isOpen(row - 1, col))
+        if (row > 1 && isOpen(row - 1, col)) {
             sites.union(rowColTo1D(row, col), rowColTo1D(row - 1, col));
+            sitesViz.union(rowColTo1D(row, col), rowColTo1D(row - 1, col));
+        }
+
     }
 
     private void connectBottomSite(int row, int col) {
-        if (row < gridSize && isOpen(row + 1, col))
+        if (row < gridSize && isOpen(row + 1, col)) {
             sites.union(rowColTo1D(row, col), rowColTo1D(row + 1, col));
+            sitesViz.union(rowColTo1D(row, col), rowColTo1D(row + 1, col));
+        }
+
     }
 }
---
+++ PercolationStats.java
@@ -0,0 +1,89 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+/**
+ * Created by USUARIO on 21/02/2017.
+ */
+public class PercolationStats {
+
+    double[] openSitePercent;
+    double z = 1.96;
+    int n, trials, siteNumber;
+
+
+    /**
+     * perform trials independent experiments on an n-by-n grid
+     *
+     * @param n      to calculate grid size nxn
+     * @param trials Number of times to run the experiment
+     */
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("N and trials must be greater than 0");
+        }
+        this.n = n;
+        this.siteNumber = n * n;
+        this.trials = trials;
+        openSitePercent = new double[trials];
+    }
+
+    /**
+     * Test client
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int n = new Integer(args[0]);
+        int trials = new Integer(args[0]);
+
+        PercolationStats pStats = new PercolationStats(n, trials);
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                int row, col;
+                do {
+                    row = StdRandom.uniform(1, n + 1);
+                    col = StdRandom.uniform(1, n + 1);
+                } while (p.isOpen(row, col));
+
+                p.open(row, col);
+            }
+            pStats.openSitePercent[i] = p.numberOfOpenSites() / ((double) pStats.siteNumber);
+        }
+        System.out.println(String.format("%-23s = %f", "mean", pStats.mean()));
+        System.out.println(String.format("%-23s = %f", "stddev", pStats.stddev()));
+        System.out.println(String.format("%-23s = [%f, %f]", "95% confidence interval", pStats.confidenceLo(), pStats.confidenceHi()));
+
+    }
+
+    /**
+     * @return sample mean of percolation threshold
+     */
+    public double mean() {
+        return StdStats.mean(this.openSitePercent);
+    }
+
+    /**
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        if (trials == 1) {
+            return Double.NaN;
+        }
+        return StdStats.stddev(this.openSitePercent);
+    }
+
+    /**
+     * @return low  endpoint of 95% confidence interval
+     */
+    public double confidenceLo() {
+        return mean() - (z * (stddev() / Math.sqrt(n)));
+    }
+
+    /**
+     * @return high endpoint of 95% confidence interval
+     */
+    public double confidenceHi() {
+        return mean() + (z * (stddev() / Math.sqrt(n)));
+    }
+}
